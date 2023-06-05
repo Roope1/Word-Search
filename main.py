5fce94ed5928a1a160f91a313cf9c9a558fba448
Roope1 <roopeh.turkki@gmail.com>
2023-06-05 19:52:01 +0300

    works

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    pass
+from time import perf_counter
+from trie import trieDS
 
+def main():
+    
+    print("Initializing data-structure")
+    init_start_time = perf_counter()
+    trie = trieDS()
+    with open("words_alpha.txt", "r") as f:
+        lines = f.readlines()
+
+    for word in lines:
+        trie.add_word(word.strip())
+    word_count = len(lines)
+    del lines
+    init_fin_time = perf_counter()
+
+    print(f"Initialization ready. Took {init_fin_time - init_start_time} seconds")
+    print(f"Added {word_count} words.")
+
+    while True:
+        word_to_check = input("Check if word exists: ")
+        res = trie.find_word(word_to_check)
+        if res:
+            print(f"{word_to_check} is a word!\n")
+        else:
+            print(f"{word_to_check} is not a word\n")
 
 if __name__ == "__main__":
     main()
--- trie.py
+++ trie.py
@@ -1,10 +1,9 @@
 import string
 
 class trieNode:
-    def __init__(self, value: str, is_terminal: bool = False) -> None:
-        self.children: List[trieNode] = list() 
-        self.children_vals: List[str]
-        self.is_terminal: bool = is_terminal
+    def __init__(self, value: str) -> None:
+        self.children: List[trieNode] = [] 
+        self.is_terminal: bool = False
         self.value: str = value
 
     def __str__ (self) -> str:
@@ -20,11 +19,8 @@ class trieNode:
 
 class trieDS:
     # on creation make root nodes for each alpahabet
-    roots = set()
-    
     def __init__(self) -> None:
-        for char in list(string.ascii_lowercase):
-          self.roots.add(trieNode(char))  
+        self.roots = set()
 
     def find_root(self, value: str) -> trieNode:
         for root in self.roots:
@@ -33,7 +29,10 @@ class trieDS:
 
     def add_word(self, word: str) -> None:
         # if root element doesn't exist create it
-        if word[0] not in self.roots:
+        for root in self.roots:
+            if root.value == word[0]:
+                break
+        else:
             self.roots.add(trieNode(word[0]))
 
         # get root node 
@@ -43,6 +42,7 @@ class trieDS:
         for char in list(word[1:]):
             curr_node = curr_node.get_child(char)
 
+        # last node marked as terminal
         curr_node.is_terminal = True
 
     def find_word(self, word:str) -> bool:
@@ -56,4 +56,5 @@ class trieDS:
         
         if not curr_node.is_terminal:
             return False
+            
         return True
